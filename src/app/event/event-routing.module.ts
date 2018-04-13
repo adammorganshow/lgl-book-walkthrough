b2311b069b57c6482e6535fa484328e9b52de683
Adam Morgan <atom.morgan@gmail.com>
2018-04-13 15:52:41 -0400

    Add event guard

--- src/app/event/event-routing.module.ts
+++ src/app/event/event-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from '../guards/auth/auth.guard';
 import { EventCreateComponent } from './event-create/event-create.component';
 import { EventViewComponent } from './event-view/event-view.component';
 import { EventUpdateComponent } from './event-update/event-update.component';
+import { EventGuard } from '../guards/event/event.guard';
 
 const routes: Routes = [
   { path: '', component: EventCreateComponent, canActivate: [AuthGuard] },
@@ -11,12 +12,13 @@ const routes: Routes = [
   {
     path: ':id/update',
     component: EventUpdateComponent,
-    canActivate: [AuthGuard]
+    canActivate: [EventGuard, AuthGuard]
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [EventGuard]
 })
 export class EventRoutingModule { }

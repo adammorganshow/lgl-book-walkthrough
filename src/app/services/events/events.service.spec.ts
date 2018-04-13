e2da72661dd36f3ca04e2a8599e487d67423d1aa
Adam Morgan <atom.morgan@gmail.com>
2018-04-13 14:43:32 -0400

    Add event subscribe

--- src/app/services/events/events.service.spec.ts
+++ src/app/services/events/events.service.spec.ts
@@ -3,8 +3,18 @@ import {
   HttpClientTestingModule,
   HttpTestingController
 } from '@angular/common/http/testing';
+import { AuthService } from '../auth/auth.service';
 import { EventsService } from './events.service';
 import { Event } from './event';
+
+class MockAuthService {
+  currentUser = jasmine.createSpy('currentUser').and.callFake(() => {
+    return {
+      'username': 'johndoe',
+      '_id': '58dab4f21342131b8c96787f'
+    };
+  });
+}
 
 describe('EventsService', () => {
   let eventsService: EventsService;
@@ -13,7 +23,10 @@ describe('EventsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [EventsService]
+      providers: [
+        EventsService,
+        { provide: AuthService, useClass: MockAuthService }
+      ]
     });
 
     eventsService = TestBed.get(EventsService);
@@ -233,6 +246,79 @@ describe('EventsService', () => {
 
       http
         .expectOne('http://localhost:8080/api/events')
+        .flush({message: error}, {status: 500, statusText: 'Server Error'});
+      expect(errorResponse.error.message).toEqual(error);
+      http.verify();
+    });
+  });
+
+  describe('isEventCreator', () => {
+    it('should return true if the event creator is the current user', () => {
+      const id = '58dab4f21342131b8c96787f';
+      expect(eventsService.isEventCreator(id)).toEqual(true);
+    });
+
+    it('should return false if the event creator is not the current user', () => {
+      const id = '12345';
+      expect(eventsService.isEventCreator(id)).toEqual(false);
+    });
+  });
+
+  describe('subscribe', () => {
+    it('should return an event with an updated members list', () => {
+      const eventId = '5a55135639fbc4ca3ee0ce5a';
+      const subscriber = { user: '5a539449b689d341cccc4be7' };
+      const subscribeResponse: Event = {
+        '_id': '5a55135639fbc4ca3ee0ce5a',
+        '_creator': '5a550ea739fbc4ca3ee0ce58',
+        'title': 'My first updated event',
+        'description': 'My first updated description',
+        'city': 'Miami',
+        'state': 'FL',
+        'startTime': '2018-01-09T19:00:00.000Z',
+        'endTime': '2018-01-09T20:00:00.000Z',
+        '__v': 1,
+        'suggestLocations': true,
+        'members': [
+          {
+            '_id': '5a550ea739fbc4ca3ee0ce58',
+            'username': 'newUser',
+            '__v': 0,
+            'dietPreferences': []
+          },
+          {
+            '_id': '5a539449b689d341cccc4be7',
+            'username': 'adam',
+            '__v': 0,
+            'dietPreferences': []
+          }
+        ]
+      };
+      let response;
+
+      eventsService.subscribe(eventId, subscriber).subscribe(res => {
+        response = res;
+      });
+
+      http
+        .expectOne('http://localhost:8080/api/events/' + eventId + '/subscribe')
+        .flush(subscribeResponse);
+      expect(response).toEqual(subscribeResponse);
+      http.verify();
+    });
+
+    it('should return an error if a user cannot be subscribed to an event', () => {
+      const eventId = '5a55135639fbc4ca3ee0ce5a';
+      const subscriber = { user: '5a539449b689d341cccc4be7' };
+      const error = 'Something went wrong. Try again.';
+      let errorResponse;
+
+      eventsService.subscribe(eventId, subscriber).subscribe(res => {}, err => {
+        errorResponse = err;
+      });
+
+      http
+        .expectOne('http://localhost:8080/api/events/' + eventId + '/subscribe')
         .flush({message: error}, {status: 500, statusText: 'Server Error'});
       expect(errorResponse.error.message).toEqual(error);
       http.verify();
--- src/app/services/events/events.service.ts
+++ src/app/services/events/events.service.ts
@@ -4,11 +4,12 @@ import { Event } from './event';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { format } from 'date-fns';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class EventsService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   create(event: Event): Observable<Event> {
     return this.http.post<Event>('http://localhost:8080/api/events', event);
@@ -28,10 +29,20 @@ export class EventsService {
     return this.http.get<Event[]>('http://localhost:8080/api/events');
   }
 
+  subscribe(eventId: string, user: object): Observable<Event> {
+    return this.http.patch<Event>('http://localhost:8080/api/events/' +
+                                  eventId + '/subscribe', user);
+  }
+
   formatDateTime(event: Event): Event {
     event.displayStart = format(event.startTime, 'dddd MMM, Do - h:mm A');
     event.displayEnd = format(event.endTime, 'dddd MMM, Do - h:mm A');
     return event;
+  }
+
+  isEventCreator(creatorId: string): boolean {
+    const user = this.authService.currentUser();
+    return user._id === creatorId ? true : false;
   }
 
 }
